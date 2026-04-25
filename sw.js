8fb0797955f6f746c8ac70b87c7dd628ff8cdd4f
T1pTip <45393675+T1pTip@users.noreply.github.com>
2026-04-25 23:03:02 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gym-pro-v7';
+const CACHE = 'gym-pro-v8';
 const PRECACHE = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-180.png','./icon-32.png'];
 
 self.addEventListener('install', e => {
