400190a06fbb408152868fdf47e52e5aef265964
T1pTip <45393675+T1pTip@users.noreply.github.com>
2026-04-25 23:49:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gym-pro-v11';
+const CACHE = 'gym-pro-v12';
 const PRECACHE = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-180.png','./icon-32.png'];
 
 self.addEventListener('install', e => {
@@ -13,7 +13,7 @@ self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys()
       .then(keys => Promise.all(
-        keys.filter(k => k !== CACHE && (k.startsWith('gym-pro-') || k === 'gym-pro-v2-app'))
+        keys.filter(k => k !== CACHE && (k.startsWith('gym-pro-') || k === 'gym-pro-v12-app'))
             .map(k => caches.delete(k))
       ))
       .then(() => self.clients.claim())
